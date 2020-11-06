aaaa42885293c4df630a0229c53d84839f1aac0d
Ronan Duffy <48518654+ronanduffy3@users.noreply.github.com>
2020-11-06 20:47:00 +0000

    Changed the listeners for the dropbox event to individual seperate classes.

--- app/src/main/java/com/example/resistorassignment_ronan_finnegan_duffy_s00187127/MainActivity.java
+++ app/src/main/java/com/example/resistorassignment_ronan_finnegan_duffy_s00187127/MainActivity.java
@@ -12,13 +12,14 @@ import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+public class MainActivity extends AppCompatActivity {
     // Get fields off view
     private Spinner band1Spinner, band2Spinner, bandMultiplier, bandTolerance;
     private ImageView ivBand1, ivBand2, ivBandMultiplier, ivBandTolerance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -55,63 +56,178 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         bandMultiplier.setAdapter(adapter2);
         bandTolerance.setAdapter(adapter3);
 
-        band1Spinner.setOnItemSelectedListener(this);
+        band1Spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String text = parent.getItemAtPosition(position).toString();
+                switch (text) {
+                    case "Black":
+                        ivBand1.setImageDrawable(null);
+                        ivBand1.setBackgroundResource(R.drawable.black);
+                        break;
+                    case "Brown":
+                        ivBand1.setImageDrawable(null);
+                        ivBand1.setBackgroundResource(R.drawable.brown);
+                        break;
+                    case "Red":
+                        ivBand1.setImageDrawable(null);
+                        ivBand1.setBackgroundResource(R.drawable.red);
+                        break;
+                    case "Orange":
+                        ivBand1.setImageDrawable(null);
+                        ivBand1.setBackgroundResource(R.drawable.orange);
+                        break;
+                    case "Yellow":
+                        ivBand1.setImageDrawable(null);
+                        ivBand1.setBackgroundResource(R.drawable.yellow);
+                        break;
+                    case "Green":
+                        ivBand1.setImageDrawable(null);
+                        ivBand1.setBackgroundResource(R.drawable.green);
+                        break;
+                    case "Blue":
+                        ivBand1.setImageDrawable(null);
+                        ivBand1.setBackgroundResource(R.drawable.blue);
+                        break;
+                    case "Violet":
+                        ivBand1.setImageDrawable(null);
+                        ivBand1.setBackgroundResource(R.drawable.violet);
+                        break;
+                    case "Grey":
+                        ivBand1.setImageDrawable(null);
+                        ivBand1.setBackgroundResource(R.drawable.grey);
+                        break;
+                    case "White":
+                        ivBand1.setImageDrawable(null);
+                        ivBand1.setBackgroundResource(R.drawable.white);
+                        break;
+                    default:
+                        System.out.println("Default");
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        band2Spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String text = parent.getItemAtPosition(position).toString();
+                switch (text) {
+                    case "Black":
+                        ivBand2.setImageDrawable(null);
+                        ivBand2.setBackgroundResource(R.drawable.black);
+                        break;
+                    case "Brown":
+                        ivBand2.setImageDrawable(null);
+                        ivBand2.setBackgroundResource(R.drawable.brown);
+                        break;
+                    case "Red":
+                        ivBand2.setImageDrawable(null);
+                        ivBand2.setBackgroundResource(R.drawable.red);
+                        break;
+                    case "Orange":
+                        ivBand2.setImageDrawable(null);
+                        ivBand2.setBackgroundResource(R.drawable.orange);
+                        break;
+                    case "Yellow":
+                        ivBand2.setImageDrawable(null);
+                        ivBand2.setBackgroundResource(R.drawable.yellow);
+                        break;
+                    case "Green":
+                        ivBand2.setImageDrawable(null);
+                        ivBand2.setBackgroundResource(R.drawable.green);
+                        break;
+                    case "Blue":
+                        ivBand2.setImageDrawable(null);
+                        ivBand2.setBackgroundResource(R.drawable.blue);
+                        break;
+                    case "Violet":
+                        ivBand2.setImageDrawable(null);
+                        ivBand2.setBackgroundResource(R.drawable.violet);
+                        break;
+                    case "Grey":
+                        ivBand2.setImageDrawable(null);
+                        ivBand2.setBackgroundResource(R.drawable.grey);
+                        break;
+                    case "White":
+                        ivBand2.setImageDrawable(null);
+                        ivBand2.setBackgroundResource(R.drawable.white);
+                        break;
+                    default:
+                        System.out.println("Default");
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+
+
 
     }
 
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        String text = parent.getItemAtPosition(position).toString();
-        switch(text){
-            case "Black":
-                ivBand1.setImageDrawable(null);
-                ivBand1.setBackgroundResource(R.drawable.black);
-                break;
-            case "Brown":
-                ivBand1.setImageDrawable(null);
-                ivBand1.setBackgroundResource(R.drawable.brown);
-                break;
-            case "Red":
-                ivBand1.setImageDrawable(null);
-                ivBand1.setBackgroundResource(R.drawable.red);
-                break;
-            case "Orange":
-                ivBand1.setImageDrawable(null);
-                ivBand1.setBackgroundResource(R.drawable.orange);
-                break;
-            case "Yellow":
-                ivBand1.setImageDrawable(null);
-                ivBand1.setBackgroundResource(R.drawable.yellow);
-                break;
-            case "Green":
-                ivBand1.setImageDrawable(null);
-                ivBand1.setBackgroundResource(R.drawable.green);
-                break;
-            case "Blue":
-                ivBand1.setImageDrawable(null);
-                ivBand1.setBackgroundResource(R.drawable.blue);
-                break;
-            case "Violet":
-                ivBand1.setImageDrawable(null);
-                ivBand1.setBackgroundResource(R.drawable.violet);
-                break;
-            case "Grey":
-                ivBand1.setImageDrawable(null);
-                ivBand1.setBackgroundResource(R.drawable.grey);
-                break;
-            case "White":
-                ivBand1.setImageDrawable(null);
-                ivBand1.setBackgroundResource(R.drawable.white);
-                break;
-            default:
-                System.out.println("Default");
-        }
 
-        System.out.println(text);
-    }
 
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
-    }
+//    @Override
+//    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//        String text = parent.getItemAtPosition(position).toString();
+//        switch(text){
+//            case "Black":
+//                ivBand1.setImageDrawable(null);
+//                ivBand1.setBackgroundResource(R.drawable.black);
+//                break;
+//            case "Brown":
+//                ivBand1.setImageDrawable(null);
+//                ivBand1.setBackgroundResource(R.drawable.brown);
+//                break;
+//            case "Red":
+//                ivBand1.setImageDrawable(null);
+//                ivBand1.setBackgroundResource(R.drawable.red);
+//                break;
+//            case "Orange":
+//                ivBand1.setImageDrawable(null);
+//                ivBand1.setBackgroundResource(R.drawable.orange);
+//                break;
+//            case "Yellow":
+//                ivBand1.setImageDrawable(null);
+//                ivBand1.setBackgroundResource(R.drawable.yellow);
+//                break;
+//            case "Green":
+//                ivBand1.setImageDrawable(null);
+//                ivBand1.setBackgroundResource(R.drawable.green);
+//                break;
+//            case "Blue":
+//                ivBand1.setImageDrawable(null);
+//                ivBand1.setBackgroundResource(R.drawable.blue);
+//                break;
+//            case "Violet":
+//                ivBand1.setImageDrawable(null);
+//                ivBand1.setBackgroundResource(R.drawable.violet);
+//                break;
+//            case "Grey":
+//                ivBand1.setImageDrawable(null);
+//                ivBand1.setBackgroundResource(R.drawable.grey);
+//                break;
+//            case "White":
+//                ivBand1.setImageDrawable(null);
+//                ivBand1.setBackgroundResource(R.drawable.white);
+//                break;
+//            default:
+//                System.out.println("Default");
+//        }
+//
+//        System.out.println(text);
+//    }
+//
+//    @Override
+//    public void onNothingSelected(AdapterView<?> parent) {
+//
+//    }
 }
+
