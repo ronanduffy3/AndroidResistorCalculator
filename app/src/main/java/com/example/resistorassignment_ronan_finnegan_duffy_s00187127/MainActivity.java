d274fb3be394590febffbdd3758acf6f3706469b
Ronan Duffy <48518654+ronanduffy3@users.noreply.github.com>
2020-11-06 19:54:39 +0000

    Changed the way I set the drawables for the image view. Still need to add method to change the second third and fourth imageview when the item is selected.

--- app/src/main/java/com/example/resistorassignment_ronan_finnegan_duffy_s00187127/MainActivity.java
+++ app/src/main/java/com/example/resistorassignment_ronan_finnegan_duffy_s00187127/MainActivity.java
@@ -3,16 +3,19 @@ package com.example.resistorassignment_ronan_finnegan_duffy_s00187127;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
     // Get fields off view
     private Spinner band1Spinner, band2Spinner, bandMultiplier, bandTolerance;
+    private ImageView ivBand1, ivBand2, ivBandMultiplier, ivBandTolerance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +27,11 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         band2Spinner = findViewById(R.id.band2Spinner);
         bandMultiplier = findViewById(R.id.multiplierSpinner);
         bandTolerance = findViewById(R.id.toleranceSpinner);
+
+        ivBand1 = findViewById(R.id.bandOne);
+        ivBand2 = findViewById(R.id.bandTwo);
+        ivBandMultiplier = findViewById(R.id.bandMultiplier);
+        ivBandTolerance = findViewById(R.id.bandTolerance);
 
         // Create an array adaptor for the first two spinners
         ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,
@@ -54,6 +62,51 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         String text = parent.getItemAtPosition(position).toString();
+        switch(text){
+            case "Black":
+                ivBand1.setImageDrawable(null);
+                ivBand1.setBackgroundResource(R.drawable.black);
+                break;
+            case "Brown":
+                ivBand1.setImageDrawable(null);
+                ivBand1.setBackgroundResource(R.drawable.brown);
+                break;
+            case "Red":
+                ivBand1.setImageDrawable(null);
+                ivBand1.setBackgroundResource(R.drawable.red);
+                break;
+            case "Orange":
+                ivBand1.setImageDrawable(null);
+                ivBand1.setBackgroundResource(R.drawable.orange);
+                break;
+            case "Yellow":
+                ivBand1.setImageDrawable(null);
+                ivBand1.setBackgroundResource(R.drawable.yellow);
+                break;
+            case "Green":
+                ivBand1.setImageDrawable(null);
+                ivBand1.setBackgroundResource(R.drawable.green);
+                break;
+            case "Blue":
+                ivBand1.setImageDrawable(null);
+                ivBand1.setBackgroundResource(R.drawable.blue);
+                break;
+            case "Violet":
+                ivBand1.setImageDrawable(null);
+                ivBand1.setBackgroundResource(R.drawable.violet);
+                break;
+            case "Grey":
+                ivBand1.setImageDrawable(null);
+                ivBand1.setBackgroundResource(R.drawable.grey);
+                break;
+            case "White":
+                ivBand1.setImageDrawable(null);
+                ivBand1.setBackgroundResource(R.drawable.white);
+                break;
+            default:
+                System.out.println("Default");
+        }
+
         System.out.println(text);
     }
 
